6a1ba07bba5039f53261c9d1b015e26123e6ce11
utkarsh15153977 <60875408+utkarsh15153977@users.noreply.github.com>
2022-07-23 16:26:17 +0530

    Searching element in an lsorted array in descending order

---
+++ Binary Tree/SearchDesc.cpp
@@ -0,0 +1,47 @@
+//Binary Search Tree (Searching element in an lsorted array in descending order) 
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n;
+    cout<<"Enter the size of array."<<endl;
+    cin>>n;
+    ll a[n];
+    cout<<"Enter element in array."<<endl;
+    for(ll i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    ll start=0;
+    ll last=n-1;
+    ll mid=0;
+    ll ele;
+    bool b1=false;
+    cout<<"Enter the element you want to search the index."<<endl;
+    cin>>ele;
+    while(start<=last)
+    {
+        mid=(start+last)/2;
+        if(ele==a[mid])
+        {
+            break;
+        }
+        else if(ele<a[mid])
+        {
+            start=mid+1;
+        }
+        else if(ele>a[mid])
+        {
+            last=mid-1;
+        }
+    }
+    cout<<"The Index of the element is "<<abs(mid);
+    return 0;
+}
