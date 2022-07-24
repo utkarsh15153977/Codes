27e76ee7bf2576cec98e2af757266b80f70e0ace
utkarsh15153977 <60875408+utkarsh15153977@users.noreply.github.com>
2022-07-25 00:45:29 +0530

    Create Crazy_Walk.cpp
    Problem Statement
    Sky (the blue ranger) wants to replace Jack (the red ranger) from his position. For this, he needs to conquer the entire Base.
    The Base can be represented as an M*N grid, and Sky needs to conquer it cell by cell. Sky starts conquering the Base from the cell (1, 1). In each move, he conquers the cell, and moves to an adjacent non- conquered cell (he cannot move if there is no adjacent non- conquered cell). Now, there is a catch, the last cell he needs to conquer is (M, N) so as to complete the quest for the red ranger tag!
    
    Please let us know if Sky can replace Jack by conquering all the cells in the Base.
    
    Note: The diagonal cells are not considered as adjacent cells.
    Input
    The first and the only line of input contains two integers M and N.
    
    Constraints
    1 <= M, N <= 1000
    Output
    Output "YES" (without quotes) if Sky can conquer the entire Base to replace Jack, else output "NO" (without quotes).
    Example
    Sample Input
    2 2
    
    Sample Output
    NO
    
    Explanation
    The possible journeys of Sky ending at (2, 2) can be:
    (1, 1) - > (1, 2) - > (2, 2)
    (1, 1) - > (2, 1) - > (2, 2)
    Since, in each of the path that Sky takes, the total cells covered are not 4, hence Sky cannot conquer the entire base.
    
    Sample Input
    3 3
    
    Sample Output
    YES

---
+++ Newton School Codes/Coding Contest July 2022/Crazy_Walk.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	// Your code here
+    int m,n;
+    cin>>m>>n;
+    if(m%2==0&&n%2==0){
+        cout<<"NO";
+    }
+    else{
+        cout<<"YES";
+    }
+    return 0;
+}
