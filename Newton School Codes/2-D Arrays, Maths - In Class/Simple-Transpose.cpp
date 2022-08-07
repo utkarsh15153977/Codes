326a56274cf08a802007c03d07705edc3659ee22
utkarsh15153977 <60875408+utkarsh15153977@users.noreply.github.com>
2022-08-07 13:48:14 +0530

    Update Simple-Transpose.cpp

--- Newton School Codes/2-D Arrays, Maths - In Class/Simple-Transpose.cpp
+++ Newton School Codes/2-D Arrays, Maths - In Class/Simple-Transpose.cpp
@@ -1,3 +1,42 @@
+/*
+
+Problem Statement
+You are given a NxN matrix. You need to find the transpose of the matrix.
+The matrix is of form:
+a b c ...
+d e f ...
+g h i ...
+...........
+There are N elements in each row.
+Input
+The first line of the input contains an integer N denoting the size of the square matrix.
+The next N lines contain N single-spaced integers.
+
+Constraints
+1 <= N <= 100
+1 <=Ai <= 100000
+Output
+Output the transpose of the matrix in similar format as that of the input.
+Example
+Sample Input
+2
+1 3
+2 2
+
+Sample Output
+1 2
+3 2
+
+Sample Input:
+1 2
+3 4
+
+Sample Output:
+1 3
+2 4
+
+*/
+
 #include <bits/stdc++.h> // header file includes every Standard library
 using namespace std;
 
