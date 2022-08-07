5ece6bf63ca36fc66d4cc84cb601f373cdfff6ba
utkarsh15153977 <60875408+utkarsh15153977@users.noreply.github.com>
2022-08-07 13:09:58 +0530

    Update Maximum_difference_array.cpp

--- Newton School Codes/1 D Array/Maximum_difference_array.cpp
+++ Newton School Codes/1 D Array/Maximum_difference_array.cpp
@@ -1,3 +1,39 @@
+/*
+
+Problem Statement
+Given an array of integers of size N, your task is to find the maximum parity index of this array.
+Parity Index is the maximum difference between two indices i and j (1 <= i <= j <= N) of an array A such that Ai < Aj.
+Input
+First line of contains a single integer N, next line contains N space separated integers depicting the values of array.
+
+Constraints:-
+1 < = N < = 100000
+1 < = Arr[i] < = 100000
+Output
+Print the maximum value of j- i under the given condition, if no pair satisfies the condition print -1.
+Example
+Sample Input:-
+5
+1 2 3 4 5
+
+Sample Output:-
+4
+
+Explanation:
+The maximum difference of jth - ith index is 4:(4th - 0th), also arr[4] > arr[0]
+
+
+Sample Input:-
+5
+5 4 3 2 1
+
+Sample Output:-
+-1
+
+*/
+
+
+
 #include <bits/stdc++.h> // header file includes every Standard library
 using namespace std;
  
